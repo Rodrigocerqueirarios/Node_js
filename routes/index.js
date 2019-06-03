44bc8735e1d1f072f9c5b09cb1cc94395d81649b
Rodrigo Rios <rodrigo.cerqueira.rios@hotmail.com>
2019-06-02 21:28:54 -0300

    trabalhando com Express

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Rodrigo Rios' });
+});
+
+/* GET new page. */
+router.get('/new', function(req, res, next) {
+  res.render('new', { title: 'Novo Cadastro' });
+});
+
+/* Post new page. */
+router.post('/new', function(req, res, next) {
+  var nome = req.body.nome;
+  var idade = req.body.idade;
+  res.redirect('/new?nome=' + nome);
 });
 
 module.exports = router;
